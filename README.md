b54ae18c2c837077ef82825d66bf7c49973a7b8f
Hoodoo42 <create@hoodooverse.ca>
2022-11-03 21:20:30 -0600

    added a few basic starter methods for the structure of the gameplay/game space. As well as some paper planning away from coding.

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Created skeleton directory for mvp_front
 creating my first full js game. As it is only the very beginning I plan on there being many many changes and trial and error as I go.  
 
 My Notes:
+phaser.js is the fully/commented out version of phaser.min js, and wont be used for production.
 phaser.min.js is the phaser framework
 
 
---
+++ js/game.js
@@ -0,0 +1,20 @@
+
+
+// creating a scene. This scene will contain the entire game. This scene has three methods
+
+class Main{
+
+    // preload is called ONCE at the begining. Will load assets
+    preload(){
+
+    }
+
+    // create is called ONCE, AFTER preload. will initialize the scene(sprite position etc)
+    create(){
+
+    }
+    // update is called 60 times/second  after create and handles all the games logic like movements
+    update() {
+
+    }
+}
